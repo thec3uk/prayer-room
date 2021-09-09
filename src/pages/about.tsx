d048fe290b9f7b983057ab186a3e6b0ed76d63b3
Andrew Miller <info@akmiller.co.uk>
2021-09-09 09:35:20 +0100

    update about layout

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,8 +4,8 @@ import Layout from "../components/layout";
 const PrayerMeetupPage = () => {
   return (
     <Layout title={"The Tim Creamer Prayer Room"}>
-      <div className="md:h-screen md:flex md:flex-col md:justify-center md:items-start">
-        <div className="w-screen md:w-full px-4 py-2 text-lg md:text-2xl font-sans mb-8">
+      <div className="md:h-screen md:flex md:flex-col md:justify-center md:items-start md:mx-32">
+        <div className="w-screen md:w-full px-4 py-2 text-lg md:text-lg font-sans mb-8">
           This space was created in honour of Tim Creamer. A man of passionate faith who was always first to pray. Sadly
           missed but an inspiration to us all to ‘run the race with perseverance’.
         </div>
