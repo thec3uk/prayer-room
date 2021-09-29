dc504ff0a5066af000a80c7a3a0e7fc0477a0099
Andrew Miller <info@akmiller.co.uk>
2021-09-29 16:16:07 +0100

    add prayer praise to netlify take 2

--- src/components/prayerPraise.tsx
+++ src/components/prayerPraise.tsx
@@ -16,6 +16,7 @@ export default function PrayerPraiseToggle({ enabled, setEnabled }) {
       <Switch
         checked={enabled}
         onChange={setEnabled}
+        name="is_praise"
         className={classNames(
           enabled ? "bg-blue-400" : "bg-blue-700",
           "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
