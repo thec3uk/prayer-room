b376fdd8e4aec4c66bd73248d2e1e933640852e5
Andrew Miller <info@akmiller.co.uk>
2021-09-30 21:03:20 +0100

    prayer/praise take 5

--- src/pages/request.tsx
+++ src/pages/request.tsx
@@ -91,7 +91,7 @@ const PrayerRequestPage = () => {
             onSubmit={handleSubmit}
             data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="prayer-request" />{" "}
-            <input type="hidden" name="is_praise" value={isPraise.toString()} />
+            <input type="hidden" name="is_praise" value={`${isPraise.toString()}`} />
             <p hidden>
               <label>
                 Don’t fill this out: <input name="bot-field" onChange={handleChange} />
