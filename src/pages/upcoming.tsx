0e04fc80cd5bb2285e9dc9ef18510798ff50da7a
Andrew Miller <info@akmiller.co.uk>
2022-01-08 11:28:32 +0000

    add button for fasting meeting

--- src/pages/upcoming.tsx
+++ src/pages/upcoming.tsx
@@ -8,20 +8,33 @@ import GroupedList from "../components/groupedList";
 import BoxLink from "../components/boxLink";
 
 const PrayerMeetupPage = ({ data }) => {
+  const groupCall = true;
   return (
     <Layout
       title={"join a meetup"}
       menu={
         <>
-          <div className="w-full -mb-4 mt-6 z-50">
+          <div className="z-50 w-full mt-6 -mb-4">
             <BoxLink title="Schedule a meeting" alignment="right" to="/meetup" />
           </div>
         </>
       }>
       <div>
-        <div className="w-screen md:w-full bg-gray-600 text-gray-50 px-4 py-2 text-lg font-sans shadow mb-24">
+        <div
+          className={`w-screen px-4 py-2 ${
+            groupCall ? "mb-12" : "mb-24"
+          } font-sans text-lg bg-gray-600 shadow md:w-full text-gray-50`}>
           See upcoming prayer meetups
         </div>
+        {groupCall && (
+          <div className="w-screen px-4 mb-8 text-lg font-extrabold text-center text-black md:w-full">
+            <a href="https://zoom.us/j/6218249928?pwd=MnhyenA2clIrS0IwNEZ5b3hGMldMZz09">
+              <h3 className="block p-4 transform bg-blue-400 shadow hover:translate-y-px hover:shadow-sm hover:text-gray-50 hover:bg-blue-600">
+                Zoom Call for January Prayer &amp; Fasting
+              </h3>
+            </a>
+          </div>
+        )}
         <GroupedList group={data.allScheduledEvent.group} Component={MeetupCard} />
       </div>
     </Layout>
