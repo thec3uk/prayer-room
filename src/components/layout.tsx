5f88b5f5eb9c58f3ccaa777bd717709a19e8caa5
Andrew Miller <info@akmiller.co.uk>
2022-01-12 10:24:45 +0000

    fix layout when scrolling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -23,10 +23,10 @@ const Layout: React.FC<PageProps> = ({
   return (
     <>
       <SEO title={seoTitle} />
-      <div className="font-serif absolute insert-0 bg-gray-50 w-screen flex flex-row ">
-        <div className="min-h-screen flex flex-col justify-start items-start pt-28 w-screen md:min-w-min md:w-1/5 md:border-r md:shadow overflow-y-hidden h-screen">
+      <div className="absolute flex flex-row w-screen font-serif insert-0 bg-gray-50 ">
+        <div className="flex flex-col items-start justify-start w-screen h-screen min-h-screen overflow-y-hidden pt-28 md:min-w-min md:w-1/5 md:border-r md:shadow">
           <div className="px-4">
-            <h1 className="text-6xl mb-6 text-gray-900">{title}</h1>
+            <h1 className="mb-6 text-6xl text-gray-900">{title}</h1>
             {showsubTitle && (
               <AniLink
                 swipe
@@ -34,21 +34,23 @@ const Layout: React.FC<PageProps> = ({
                 direction="left"
                 entryOffset={80}
                 to={"/about"}
-                className="text-xl underline uppercase font-sans text-gray-900">
+                className="font-sans text-xl text-gray-900 underline uppercase">
                 The Tim Creamer Prayer Room
               </AniLink>
             )}
           </div>
-          <div className="block md:hidden overflow-y-scroll">{children}</div>
-          <div className="my-auto md:block w-full">{menu}</div>
-          <p className="font-sans m-4 text-sm">
+          <div className="block overflow-y-scroll md:hidden">{children}</div>
+          <div className="w-full my-auto md:block">{menu}</div>
+          <p className="m-4 font-sans text-sm">
             If you have any issues using this site then please contact{" "}
-            <a href="mailto:prayer@thec3.uk" className="select-all underline text-blue-700">
+            <a href="mailto:prayer@thec3.uk" className="text-blue-700 underline select-all">
               prayer@thec3.uk
             </a>
           </p>
         </div>
-        <div className="hidden md:block md:w-4/5 md:bg-pattern">{children}</div>
+        <div className="hidden md:block md:w-4/5 md:bg-pattern">
+          <div className="h-screen overflow-scroll">{children}</div>
+        </div>
       </div>
       {!isHome && (
         <AniLink
@@ -57,8 +59,8 @@ const Layout: React.FC<PageProps> = ({
           direction="right"
           entryOffset={80}
           to={"/"}
-          className="fixed top-0 shadow border bg-gray-50">
-          <ChevronLeftIcon className="h-12 w-12 text-gray-900" />
+          className="fixed top-0 border shadow bg-gray-50">
+          <ChevronLeftIcon className="w-12 h-12 text-gray-900" />
         </AniLink>
       )}
     </>
